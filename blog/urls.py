399533b38ce5171cb29d0187ff3917ef41b422e6
kaposts01 <estebanbravo0905@gmail.com>
2025-12-10 02:08:07 -0300

    appsCreadas

--- blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.render_articles, name='articles')
-]
--- kapos_prj/urls.py
+++ kapos_prj/urls.py
@@ -3,6 +3,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('articles/', include('blog.urls')),
+
+    path('api/usuarios/', include('usuarios.urls')),
+    path('api/clientes/', include('clientes.urls')),
+    path('api/planes/', include('planes.urls')),
+    path('api/suscripciones/', include('suscripciones.urls')),
+    path('api/captacion/', include('captacion.urls')),
+    path('api/pagos/', include('pagos.urls')),
+    path('api/auditoria/', include('auditoria.urls')),
 ]
